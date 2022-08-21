1c97846172bb80b76a248a81adb8d83188c760f1
Sakshi Bobade <bobadesakshi21@gmail.com>
2022-08-21 07:27:51 +0530

    Child to parent component communication

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import './ExpenseForm.css'
 
-const ExpenseForm = () => {
+const ExpenseForm = (props) => {
   const [enteredTitle, setEnteredTitle] = useState('');
   const [enteredAmount, setEnteredAmount] = useState('');
   const [enteredDate, setenteredDate] = useState('');
@@ -25,6 +25,7 @@ const ExpenseForm = () => {
       amount: enteredAmount,
       date: enteredDate
     }
+    props.onSaveExpenseData(expenseData);
     setEnteredTitle('');
     setEnteredAmount('');
     setenteredDate('');
