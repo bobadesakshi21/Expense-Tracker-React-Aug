607602d7c925cc494902eeead8fafb37f1553b4f
Sakshi Bobade <bobadesakshi21@gmail.com>
2022-08-22 08:21:33 +0530

    Adding key prop to the list of the items

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -14,7 +14,7 @@ function Expenses(props) {
           <Card className="expenses">
             <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
             {props.items.map(expense => (
-              <ExpenseItem title={expense.title} amount={expense.amount} date={expense.date} />
+              <ExpenseItem key={expense.id} title={expense.title} amount={expense.amount} date={expense.date} />
             ))}
           </Card>
         </div>
