bed215959cb090789f1748ac4cac4fcc596112c6
Sakshi Bobade <bobadesakshi21@gmail.com>
2022-08-22 08:30:58 +0530

    Applying filters on the list

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -9,11 +9,12 @@ function Expenses(props) {
     const filterChangeHandler = (year) => {
       setFilteredYear(year);
     }
+    const filteredExpenses = props.items.filter(expense => expense.date.getFullYear().toString() === filteredYear);
     return (
         <div>
           <Card className="expenses">
             <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
-            {props.items.map(expense => (
+            {filteredExpenses.map(expense => (
               <ExpenseItem key={expense.id} title={expense.title} amount={expense.amount} date={expense.date} />
             ))}
           </Card>
